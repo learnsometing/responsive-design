1a11f5b88e39150b60a9285c577331651e19f0fa
learnsometing <brianmonaccio@gmail.com>
2019-09-17 14:48:50 -0400

    Clean/organize CSS and add icon attribution

--- js/nav-toggle.js
+++ js/nav-toggle.js
@@ -1,7 +1,3 @@
-// get the nav-toggle button
-// add an event to it that adds a css class to the body of the document
-// get the nav-toggle-cancel button
-// add an event to it that removes the nav-mobile class from the body
 var body = document.querySelector("body");
 var navToggle = document.querySelector(".nav-mob-toggle");
 var toggle = () => {
